a00e7b3d0899e48c06c8e9aff4bf049a33a7bcaa
doma356 <adomaviciusdomantas@gmail.com>
2018-02-07 19:18:25 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,9 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    message.reply(message);
+    if (message.content === '!pam doma') {
+    	message.reply('http://ziezmariai.lm.lt/~detach/v2/images/file/UGDYMAS/tvarkarasciai/2018/x30014ga_add.htm');
+  	}
 });
 
 // THIS  MUST  BE  THIS  WAY
