5626db1b82a69b6ffa9776ab70a9bfa817396798
doma356 <adomaviciusdomantas@gmail.com>
2018-02-07 18:43:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,9 +6,35 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+  if (message.substring(0, 1) == '!') {
+  var args = message.substring(1).split(' ');
+  var cmd = args[0];
+  var vardas = args[1];
+
+//  logger.info(vardas);
+
+  args = args.splice(1);
+  switch(cmd) {
+      // !ping
+      case 'pam':
+          var msg;
+          if(vardas == "doma"){
+            msg = 'http://ziezmariai.lm.lt/~detach/v2/images/file/UGDYMAS/tvarkarasciai/2018/x30014ga_add.htm';
+          }
+          else if(vardas == "gytis"){
+            msg = 'http://ziezmariai.lm.lt/~detach/v2/images/file/UGDYMAS/tvarkarasciai/2018/II/x30014ga_neg.htm';
+          }
+          else{
+            msg = 'nani!?';
+          }
+          bot.sendMessage({
+              to: channelID,
+              message: msg
+          });
+      break;
+      // Just add any case commands if you want to..
+   }
+}
 });
 
 // THIS  MUST  BE  THIS  WAY
